c81bd7492570f34d89921c66ea2ea8ba25aba60c
Kyle Scheuing <kyle@kylescheuing.com>
2021-06-12 00:36:20 -0400

    Add test for backend advertiser chosen message

--- boost_request/message_generator/backend_advertiser_chosen_message_test.go
+++ boost_request/message_generator/backend_advertiser_chosen_message_test.go
@@ -1,7 +1,30 @@
 package message_generator_test
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/oppzippy/BoostRequestBot/boost_request/message_generator"
+	"github.com/oppzippy/BoostRequestBot/boost_request/repository"
+)
 
 func TestBackendAdvertiserChosenMessage(t *testing.T) {
 	t.Parallel()
+	br := &repository.BoostRequest{
+		AdvertiserID: "123",
+		Message:      "boost please!",
+		Channel:      repository.BoostRequestChannel{},
+	}
+	m := message_generator.NewBackendAdvertiserChosenMessage(emptyLocalizer(), br, "https://www.example.com")
+	message, err := m.Message()
+	if err != nil {
+		t.Errorf("error generating message: %v", err)
+		return
+	}
+	if message.Embed.Thumbnail.URL != "https://www.example.com" {
+		t.Errorf("thumbnail was not set")
+	}
+	if !strings.Contains(message.Embed.Description, "123") {
+		t.Errorf("the chosen advertiser is not mentioned in the message: %s", message.Embed.Description)
+	}
 }
