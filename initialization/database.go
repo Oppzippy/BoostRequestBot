418c91882081d9a6966b91a706c38f2c45407696
Kyle Scheuing <kyle@kylescheuing.com>
2021-11-18 04:38:52 -0500

    Replace custom dsn with protocol option and merge DB_HOST and DB_PORT

--- initialization/database.go
+++ initialization/database.go
@@ -7,18 +7,19 @@ import (
 )
 
 func GetDBC() (*sql.DB, error) {
-	dataSourceName := fmt.Sprintf(
-		"%s:%s@tcp(%s:%s)/%s",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_DATABASE"),
-	)
-	if dsn := os.Getenv("DB_DSN"); dsn != "" {
-		dataSourceName = dsn
+	protocol := os.Getenv("DB_PROTOCOL")
+	if protocol == "" {
+		protocol = "tcp"
 	}
 
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@%s(%s)/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		protocol,
+		os.Getenv("DB_ADDRESS"),
+		os.Getenv("DB_DATABASE"),
+	)
 	db, err := sql.Open("mysql", dataSourceName+"?parseTime=true")
 	if err != nil {
 		return nil, fmt.Errorf("connecting to db: %v", err)
