c6700b781802190028116b542e94f2cec5bb5582
Kyle Scheuing <kyle@kylescheuing.com>
2021-11-20 23:54:54 -0500

    Always show gold in thousands

--- boost_request/messages/message_utils/format_money.go
+++ boost_request/messages/message_utils/format_money.go
@@ -1,8 +1,6 @@
 package message_utils
 
 import (
-	"strings"
-
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"github.com/shopspring/decimal"
 )
@@ -12,61 +10,9 @@ const (
 	copperPerGold   int64 = 1_00_00
 )
 
-func FormatCopper(localizer *i18n.Localizer, totalCopper int64) string {
-	if totalCopper < copperPerGold*1000 {
-		return formatCopperToGoldSilverCopper(localizer, totalCopper)
-	} else {
-		return formatCopperToThousandsOfGold(totalCopper)
-	}
-}
-
-func formatCopperToGoldSilverCopper(localizer *i18n.Localizer, totalCopper int64) string {
-	copper := totalCopper % 100
-	silver := (totalCopper / copperPerSilver) % 100
-	gold := totalCopper / copperPerGold
-
-	parts := make([]string, 0, 3)
-
-	if gold != 0 {
-		parts = append(parts, localizer.MustLocalize(&i18n.LocalizeConfig{
-			DefaultMessage: &i18n.Message{
-				ID:    "moneyFormatGold",
-				Other: "{{.Gold}}g",
-			},
-			TemplateData: map[string]int64{
-				"Gold": gold,
-			},
-		}))
-	}
-	if silver != 0 {
-		parts = append(parts, localizer.MustLocalize(&i18n.LocalizeConfig{
-			DefaultMessage: &i18n.Message{
-				ID:    "moneyFormatSilver",
-				Other: "{{.Silver}}s",
-			},
-			TemplateData: map[string]int64{
-				"Silver": silver,
-			},
-		}))
-	}
-	if copper != 0 {
-		parts = append(parts, localizer.MustLocalize(&i18n.LocalizeConfig{
-			DefaultMessage: &i18n.Message{
-				ID:    "moneyFormatCopper",
-				Other: "{{.Copper}}c",
-			},
-			TemplateData: map[string]int64{
-				"Copper": copper,
-			},
-		}))
-	}
-
-	return strings.Join(parts, " ")
-}
-
-func formatCopperToThousandsOfGold(copper int64) string {
+func FormatCopper(localizer *i18n.Localizer, copper int64) string {
 	copperDecimal := decimal.NewFromInt(copper)
 	thousandsOfGold := copperDecimal.Div(decimal.NewFromInt(copperPerGold * 1000))
-	// TODO make configurable
+	// TODO make emoji configurable
 	return thousandsOfGold.Round(2).String() + "k <:gold:909618212717592607>"
 }
