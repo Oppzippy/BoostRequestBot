4218c473b2083b8fb0d1b32779c18f05124bf365
Kyle Scheuing <kyle@kylescheuing.com>
2022-05-01 18:36:24 -0400

    Remove test for removed feature

--- boost_request/messages/backend_signup_message_test.go
+++ boost_request/messages/backend_signup_message_test.go
@@ -1,7 +1,6 @@
 package messages_test
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/google/uuid"
@@ -42,44 +41,4 @@ func TestBackendSignupMessage(t *testing.T) {
 			t.Errorf("expected description %s, got %s", br.Message, message.Embed.Description)
 		}
 	})
-}
-
-func TestBackendSignupMessageRoleDiscount(t *testing.T) {
-	t.Parallel()
-	id, err := uuid.NewRandom()
-	if err != nil {
-		t.Errorf("generate uuid: %v", err)
-		return
-	}
-	if err != nil {
-		t.Errorf("parsing discount: %v", err)
-	}
-
-	br := &repository.BoostRequest{
-		Channel:    &repository.BoostRequestChannel{},
-		ExternalID: &id,
-	}
-	bsm := messages.NewBackendSignupMessage(
-		emptyLocalizer(),
-		br,
-		messages.BackendSignupMessageButtonConfiguration{
-			SignUp:       true,
-			Steal:        true,
-			CancelSignup: true,
-		},
-	)
-
-	message, err := bsm.Message()
-	if err != nil {
-		t.Errorf("error generating message: %v", err)
-		return
-	}
-	lines := strings.Split(message.Embed.Fields[0].Value, "\n")
-
-	if expected := "20% discount on mythic+ (booster)"; lines[0] != expected {
-		t.Errorf("Expected %s, got %s", expected, lines[0])
-	}
-	if expected := "20% discount on raid (booster)"; lines[1] != expected {
-		t.Errorf("Expected %s, got %s", expected, lines[1])
-	}
 }
